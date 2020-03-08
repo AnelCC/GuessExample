0e985ff459627ccd0f8c5897a9082d178e88dd78
Anel CC <anel.calvo.cervantes@gmail.com>
2020-03-08 17:59:22 -0500

    [Anel][GSS00006]  Make a properly encapsulated LiveData called eventGameFinish that holds a boolean

--- app/src/main/java/com/anelcc/guessexample/screen/game/GameFragment.kt
+++ app/src/main/java/com/anelcc/guessexample/screen/game/GameFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -59,9 +60,10 @@ public class GameFragment : Fragment() {
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val currentScore = viewModel.score.value ?: 0
+        /*val currentScore = viewModel.score.value ?: 0
         val action = GameFragmentDirections.actionGameToScore(currentScore)
-        findNavController().navigate(action)
+        findNavController().navigate(action)*/
+        Toast.makeText(this.activity, "Game has finished", Toast.LENGTH_SHORT).show()
     }
 
     /** Methods for updating the UI **/
